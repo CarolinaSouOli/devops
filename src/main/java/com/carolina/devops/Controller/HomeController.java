d7284949c0d2c4df9372f63e90b568bb077384e3
Carolina S Oliveira <csocarol13@gmail.com>
2021-10-09 20:03:32 -0300

    heroku

---
+++ src/main/java/com/carolina/devops/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.carolina.devops.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping(value= "/")
+    public String getMethodName() {
+        return "Olá Mundo Dev!";
+    }
+}
